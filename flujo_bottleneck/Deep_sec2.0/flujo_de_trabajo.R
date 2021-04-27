e51f730a5b2d050166394fc7808acb857f698354
TBmex <cafeinauv@gmail.com>
2021-04-27 10:15:51 +0200

    commit

--- flujo_bottleneck/Deep_sec2.0/flujo_de_trabajo.R
+++ flujo_bottleneck/Deep_sec2.0/flujo_de_trabajo.R
@@ -4,17 +4,22 @@
 library(tidyverse)
 
 # Variables
-#donor = G76.snp.rescue.final
-#receptor = G1180.snp.rescue.final
 
+# Cluster10_NO_Deep
+donor = G76.snp.rescue.final
+receptor = G1180.snp.rescue.final
+
+# Cluster04_NO_Deep
 #donor = G1180.snp.rescue.final
 #receptor = G1181.snp.rescue.final
 
+# Cluster10_Deep
 #donor = G0076BottleNeck.snp.rescue.final
 #receptor = G1180BottleNeck.snp.rescue.final
 
-donor = G1180BottleNeck.snp.rescue.final
-receptor = G1181BottleNeck.snp.rescue.final
+# Cluster04_Deep
+#donor = G1180BottleNeck.snp.rescue.final
+#receptor = G1181BottleNeck.snp.rescue.final
 
 # Extraer columnas Position y VarFreq
 donor <- donor[c(2,7)]
@@ -30,7 +35,7 @@ c <- donor %>% full_join(receptor)
 # Cambiar NAs
 c <- c %>% replace_na(list(Freq_D = 0, Freq_R = 0))
 
-# Condicionar columnas 1 Y 2
+# Condicionar columnas 1 = esta en ambos y 2 = solo esta en uno
 c <- mutate(c, SNPs = ifelse(as.integer(Freq_D * Freq_R) == 0, 2, 1))
 
 # Transformar variable
@@ -41,20 +46,29 @@ ggplot(c, aes(x=Freq_D, y=Freq_R, shape=SNPs, color=SNPs)) +
   geom_point() + geom_smooth(data=c[which(c$SNPs==1),], method=lm)
 
 # Renombramos
-Cluster04_Deep <- c
+Cluster10_NO_Deep <- c
+#Cluster04_NO_Deep <- c
+#Cluster10_Deep <- c
+#Cluster04_Deep <- c
 
 # Borramos archivos intermedios
 rm(c, donor, receptor)
 
 # Hacemos joins para ver la sposiciones que ganamos con deep secuencing.
-Win04 <- Cluster04_Deep %>% anti_join(Cluster04_NO_deep, by = "Position")
-Win10 <- Cluster10_Deep %>% anti_join(Cluster10_NO_deep, by = "Position")
+Win04 <- Cluster04_Deep %>% anti_join(Cluster04_NO_Deep, by = "Position")
+Win10 <- Cluster10_Deep %>% anti_join(Cluster10_NO_Deep, by = "Position")
 
 # Realmente ganamos estas posiciones???
 
-Aver <- Win04 %>% inner_join(G1181BottleNeck.snp.rescue.final, by = "Position")
-Aver2 <- Win04 %>% inner_join(G1180BottleNeck.snp.rescue.final, by = "Position")
-Aver3 <- Aver %>% full_join(Aver2, by = "Position")
+Ganadas1180 <- Win04 %>% inner_join(G1180BottleNeck.snp.rescue.final, by = "Position")
+Ganadas1181 <- Win04 %>% inner_join(G1181BottleNeck.snp.rescue.final, by = "Position")
+
+Ganadas1180_0 <- Ganadas1180 %>% select(Position, Freq_D, Freq_R, SNPs, Ref, Cons, VarAllele)
+Ganadas1181_0 <- Ganadas1181 %>% select(Position, Freq_D, Freq_R, SNPs, Ref, Cons, VarAllele)
+
+Aver3 <- Ganadas1180_0 %>% full_join(Ganadas1181_0)
+
+
 
 
 
